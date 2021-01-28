83fc54e71e3ceb02aa06ed16318b90511ed57fe7
Stain António Kozan AK <70895080+lemillioncorp@users.noreply.github.com>
2021-01-28 23:29:15 +0100

    Novos Aquivos
    Clone 1. 
    
    Ferramentas
    
    Alterações

---
+++ Fevereiro/Fevereiro-1/Fevereiro-2021/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Sela Smith
+ * Date: 26/01/2021
+ * Time: 14:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Fevereiro_2021
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label fechar;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.button1 = new System.Windows.Forms.Button();
+			this.fechar = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Transparent;
+			this.button1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(239, 32);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(145, 40);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Iniciar Socia";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// fechar
+			// 
+			this.fechar.BackColor = System.Drawing.Color.Transparent;
+			this.fechar.Font = new System.Drawing.Font("Harrington", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.fechar.ForeColor = System.Drawing.Color.DarkRed;
+			this.fechar.Location = new System.Drawing.Point(576, 227);
+			this.fechar.Name = "fechar";
+			this.fechar.Size = new System.Drawing.Size(28, 32);
+			this.fechar.TabIndex = 1;
+			this.fechar.Text = "X";
+			this.fechar.Click += new System.EventHandler(this.FecharClick);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+			this.ClientSize = new System.Drawing.Size(607, 260);
+			this.Controls.Add(this.fechar);
+			this.Controls.Add(this.button1);
+			this.DoubleBuffered = true;
+			this.ForeColor = System.Drawing.Color.Black;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Fevereiro-2021";
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Fevereiro/Fevereiro-1/Fevereiro-2021/MainForm.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fevereiro_2021
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+            Utilizador Uz = new Utilizador();
+            this.Hide();
+            Uz.ShowDialog();
+		}
+		void FecharClick(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
